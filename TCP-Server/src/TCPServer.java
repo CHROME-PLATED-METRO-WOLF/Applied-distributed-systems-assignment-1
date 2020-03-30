c606e5b92c852f50bcc4907b73fc65fc3e190637
caleb <caleb.davidson@cqumail.com>
2020-03-30 21:19:26 +1000

    Formatted

--- TCP-Server/src/TCPServer.java
+++ TCP-Server/src/TCPServer.java
@@ -2,6 +2,7 @@
 //package tcpserver;
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class TCPServer {
 
@@ -15,13 +16,31 @@ public class TCPServer {
             //server initialisations
             //the port wont be hardcoded in the final product it will take a command line argument
             //and will have a default value of probs 8888
-            if (args[0].compareToIgnoreCase("") == 0 || args[0] == null) {
+
+            int count = 0;
+
+            String newItem;
+            while (count < args.length) {
+                newItem = args[count].toLowerCase();
+                args[count] = newItem;
+                count++;
+            }
+
+            if (Arrays.asList(args).contains("--help") || Arrays.asList(args).contains("-h") || Arrays.asList(args).contains("-help")) {
+                //print out help file
+            }
+
+            if (Arrays.asList(args).contains("-mc") || Arrays.asList(args).contains("--max-connections")) {
+                Arrays.asList(args).indexOf("-mx".toLowerCase());
+
+            }
+
+            if (args[args.length].compareToIgnoreCase("") == 0 || args[0] == null) {
                 serverPort = 8888;
-            } else if(isPortValid(args[0])) {
+            } else if (isPortValid(args[0])) {
                 //port is a valid number
-                if(Integer.parseInt(args[0]) < 1024)
-                {
-                   System.out.println("port is less than 1024 root may be required to run on that port");
+                if (Integer.parseInt(args[0]) < 1024) {
+                    System.out.println("port is less than 1024 root may be required to run on that port");
                 }
             }
 
