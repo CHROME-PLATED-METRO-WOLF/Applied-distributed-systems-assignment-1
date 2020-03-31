cddeb1b89ba3ef7dea04167b928a478a937a0332
caleb <caleb.davidson@cqumail.com>
2020-03-31 18:46:48 +1000

    Added constructors and getters/settors to the class

--- TCP-Server/src/LogEntry.java
+++ TCP-Server/src/LogEntry.java
@@ -1,5 +1,56 @@
 
 public class LogEntry {
+String studentNumber;
+int pinCode;
+String dateTime;
 
+
+LogEntry()
+{
+    
+}
+
+LogEntry(String studentNumber, String dateTime)
+{
+    this.studentNumber = studentNumber;
+    this.dateTime = dateTime;
+}
+
+LogEntry(String studentNumber, String dateTime, int pinCode)
+{
+    this.studentNumber = studentNumber;
+    this.dateTime = dateTime;
+    this.pinCode = pinCode;
+}
+
+void setStudentNumber(String studentNumber)
+{
+    this.studentNumber = studentNumber;
+}
+
+String getStudentNumber()
+{
+    return this.studentNumber;
+}
+
+void setPinCode(int pinCode)
+{
+    this.pinCode = pinCode;
+}
+
+int getPinCode()
+{
+    return this.pinCode;
+}
+
+void setDateTime(String dateTime)
+{
+    this.dateTime = dateTime;
+}
+
+String getDatetime()
+{
+    return this.dateTime;
+}
 
 }
