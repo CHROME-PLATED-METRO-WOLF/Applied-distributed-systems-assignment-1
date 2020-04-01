393f62d64c6a811a509df32aca8fc8ec120e0edd
caleb <caleb.davidson@cqumail.com>
2020-04-01 15:42:09 +1000

    Added a maximum number of clients feature.

--- TCP-Server/src/TCPServer.java
+++ TCP-Server/src/TCPServer.java
@@ -3,15 +3,19 @@
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TCPServer {
 
     //Options options = new Options();
     final static int defaultServerPort = 8888;
-   
-   
+
     static ArrayList<Student> studentList = new ArrayList();
     static ArrayList<LogEntry> logList = new ArrayList();
+    static ArrayList<Thread> clients = new ArrayList();
     public static String studentFileName;
     public static String logFileName;
 
@@ -21,22 +25,39 @@ public class TCPServer {
         argOptions.setPredefined();
         argOptions.setProgramName("TCPServer");
         argOptions.parseOptions();
-        
+
+        ExecutorService pool = Executors.newFixedThreadPool(argOptions.getMaxConnections());
+        System.out.println("Max Threads: " + argOptions.getMaxConnections());
 
         try {
             //server initialisations
             //the port wont be hardcoded in the final product it will take a command line argument
             //and will have a default value of probs 8888
 
-           
-            ServerSocket listenSocket = new ServerSocket(argOptions.getServerPort(), argOptions.getMaxConnections());
-            
+            ServerSocket listenSocket = new ServerSocket(argOptions.getServerPort());
+
             System.out.println("Server started on port: " + listenSocket.getLocalPort());
-            System.out.println("Max connections set to: " + argOptions.getMaxConnections());
+            //System.out.println("Max connections set to: " + argOptions.getMaxConnections());
+
             while (true) {
-                Socket clientSocket = listenSocket.accept();
-                System.out.println("Client connected: " + clientSocket.getPort());
-                Connection c = new Connection(clientSocket, studentList, logList);
+
+                if (clients.size() < argOptions.getMaxConnections()) {
+                    System.out.println("Num Clients Connected: " + clients.size());
+                    Socket clientSocket = listenSocket.accept();
+                    System.out.println("Client connected: " + clientSocket.getPort());
+
+                    System.out.println("Creating thread");
+                    Connection c = new Connection(clientSocket, studentList, logList, clients);
+                    pool.execute(c);
+
+                    clients.add(c);
+                } else {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
 
         } catch (IOException e) {
@@ -44,7 +65,5 @@ public class TCPServer {
         }
 
     }
-
-    
 
 }
