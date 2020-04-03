a881ae4eab848f6dfa8a018fc5745a9cda9d444d
caleb <caleb.davidson@cqumail.com>
2020-04-03 20:14:25 +1000

    Added code to the file manager, it is incomplete. Methods for saving the files.

--- TCP-Server/src/FileManager.java
+++ TCP-Server/src/FileManager.java
@@ -1,13 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author caleb
- */
-public class FileManager {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileManager extends Thread {
+
+    private ArrayList<Student> studentList;
+    private ArrayList<LogEntry> logList;
+    private int msInterval;
+    private boolean exit = false;
+    final private String separator = " ";
+
+    FileManager(ArrayList<Student> studentList, ArrayList<LogEntry> logList) {
+        this.studentList = studentList;
+        this.logList = logList;
+    }
+
+    FileManager(ArrayList<Student> studentList, ArrayList<LogEntry> logList, int msInterval) {
+        this.studentList = studentList;
+        this.logList = logList;
+        this.msInterval = msInterval;
+    }
+
+    public void run() {
+
+    }
+
+    private void saveStudents() {
+        try {
+            File studentFile = new File("studententry.txt");
+            FileWriter studentWriter = null;
+            BufferedWriter studentBufferedWriter = null;
+
+            studentWriter = new FileWriter(studentFile);
+            studentBufferedWriter = new BufferedWriter(studentWriter);
+
+            while (exit = false) {
+                try {
+                    Thread.sleep(this.msInterval);
+                    int iterator = 0;
+                    Student currentStudent;
+                    while (iterator < studentList.size()) {
+                        currentStudent = this.studentList.get(iterator);
+                        studentBufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode());
+                    }
+
+                } catch (InterruptedException ex) {
+                    System.out.println("CRITICAL ERROR: cant sleep");
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void saveLog() {
+
+        //fix this later
+        File studentFile = new File("logentry.txt");
+        FileWriter studentWriter = null;
+        BufferedWriter studentBufferedWriter = null;
+
+        studentWriter = new FileWriter(studentFile);
+        studentBufferedWriter = new BufferedWriter(studentWriter);
+
+        while (exit = false) {
+            try {
+                Thread.sleep(this.msInterval);
+                int iterator = 0;
+                Student currentStudent;
+                while (iterator < studentList.size()) {
+                    currentStudent = this.studentList.get(iterator);
+                    studentBufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode());
+                }
+
+            } catch (InterruptedException ex) {
+                System.out.println("CRITICAL ERROR: cant sleep");
+            }
+        }
+    }
+    catch (IOException ex
+
     
+        ) {
+            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
 }
