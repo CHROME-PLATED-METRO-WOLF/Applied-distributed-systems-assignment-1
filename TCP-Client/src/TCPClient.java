d02469ae04afb91f405fe69d613a7aa531cba671
caleb <caleb.davidson@cqumail.com>
2020-04-12 00:27:57 +1000

    Added actual program code. Started coding the actual client tasks and commented out preformance test code.

--- TCP-Client/src/TCPClient.java
+++ TCP-Client/src/TCPClient.java
@@ -1,17 +1,75 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class TCPClient {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void main(String[] args) {
+        Logger logger = new Logger(true, true);
+        int option = 0;
+        String input;
+        String ipAddress;
+        int port;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket;
+
+        logger.log("Welcome to TCPClient");
+        while (true) {
+
+            logger.log("Please enter server address to connect to");
+            try {
+                ipAddress = reader.readLine();
+                logger.log("Please enter server address to connect to");
+                port = Integer.parseInt(reader.readLine());
+
+                try {
+                    clientSocket = new Socket(ipAddress, port);
+                    break;
+                } catch (IOException ex) {
+                    logger.log("Error cannot connect");
+                }
+
+            } catch (IOException ex) {
+                logger.log("ERROR cannot read input");
+            } catch (java.lang.NumberFormatException e) {
+                logger.log("Please enter a number for the port");
+            }
+
+        }
+
+        while (option != 3) {
+            logger.log("Please Make Your Selection");
+            logger.log("******************");
+            logger.log("1. Current Student");
+            logger.log("2. New Student");
+            logger.log("3. Exit");
+            logger.log("******************");
+            try {
+                input = reader.readLine();
+                option = Integer.parseInt(input);
+                if (option > 3 || option < 1) {
+                    throw new java.lang.NumberFormatException();
+                }
+            } catch (IOException ex) {
+                logger.log("ERROR cannot read input");
+            } catch (java.lang.NumberFormatException e) {
+                logger.log("Error: Please enter a number between 1 and 3");
+            }
+
+        }
+
+    }
+}
+
+//old preformance test code
+
+/*
+public class TCPClient {
+
+ 
+    
     public static void main(String[] args) {
         long randomNum = 100;
         long count = 0;
@@ -63,3 +121,4 @@ public class TCPClient {
         }
     }
 }
+ */
