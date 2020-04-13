5d5093ca46983a0304e00997b803a409b5014570
caleb <caleb.davidson@cqumail.com>
2020-04-13 18:16:20 +1000

    Updated save and load methods. To fix a lot of issues, now it runs flawlessly.

--- TCP-Server/src/FileManager.java
+++ TCP-Server/src/FileManager.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,26 +11,27 @@ import java.util.Collections;
 import java.util.List;
 
 //each time the list is saved save a hash of both lists and only if the lists have changed should they be rewritten to the file
+//you could read the 2 files in parell on other threads
 public class FileManager extends Thread {
-    
+
     private List<Student> studentList = Collections.synchronizedList(new ArrayList());
-    
+
     private List<LogEntry> logList = Collections.synchronizedList(new ArrayList());
-    
+
     private int msInterval;
     private boolean exit = false;
     final private String separator = " ";
-    
+
     FileManager(List<Student> studentList, List<LogEntry> logList, int msInterval) {
         this.studentList = studentList;
         this.logList = logList;
         this.msInterval = msInterval;
     }
-    
+
     public void run() {
-        
+
         while (exit == false) {
-            
+
             try {
                 Thread.sleep(msInterval);
                 TCPServer.logger.log("Saving lists");
@@ -40,170 +42,185 @@ public class FileManager extends Thread {
                 TCPServer.logger.log("ERROR: file manager cannot sleep thread");
             }
         }
-        
+
     }
 
     void forceSave() {
         saveFile(studentList, "studententry.txt", false);
         saveFile(logList, "logentry.txt", false);
     }
-    
+
     private void saveFile(List list, String File, boolean append) {
-        File file = new File(File);
-        FileWriter writer = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            //fix this later
-
-            writer = new FileWriter(file, append);
-            bufferedWriter = new BufferedWriter(writer);
-
-            int iterator = 0;
-
-            if (list.get(0) instanceof Student) {
-                Student currentStudent;
-                synchronized (studentList) {
-                    while (iterator < list.size()) {
-                        currentStudent = (Student) list.get(iterator);
-                        bufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode() + "\n");
-                        iterator++;
-                    }
-                }
-            } else if (list.get(0) instanceof LogEntry) {
-                LogEntry currentLog;
-                synchronized (logList) {
-                    while (iterator < list.size()) {
-                        currentLog = (LogEntry) list.get(iterator);
-                        bufferedWriter.write(currentLog.getStudentNumber() + separator + currentLog.getDatetime() + currentLog.getPinCode() + "\n");
-                        iterator++;
-                    }
-                }
-
-            } else {
-                //if this fires than it means a list of NOT Student or log entry was givin
-                TCPServer.logger.log("CRITICAL: Error both lists are not instances of");
-            }
-
-        } catch (IOException ex) {
-            TCPServer.logger.log("ERROR: IO exception when writing lists to file");
-        } catch (java.lang.IndexOutOfBoundsException e) {
-            TCPServer.logger.log("Lists are empty not saving");
-        } finally {
+        if (list.size() == 0) {
+            TCPServer.logger.log("List is empty not saving");
+        } else {
+            File file = new File(File);
+            FileWriter writer = null;
+            BufferedWriter bufferedWriter = null;
             try {
-                bufferedWriter.close();
-                writer.close();
+                //fix this later
+
+                writer = new FileWriter(file, append);
+                bufferedWriter = new BufferedWriter(writer);
+
+                int iterator = 0;
+
+                if (list.get(0) instanceof Student) {
+
+                    Student currentStudent;
+                    synchronized (studentList) {
+                        while (iterator < list.size()) {
+                            currentStudent = (Student) list.get(iterator);
+                            bufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode() + "\n");
+                            iterator++;
+                        }
+                    }
+                }
+
+                iterator = 0;
+
+                if (list.get(0) instanceof LogEntry) {
+                    LogEntry currentLog;
+                    synchronized (logList) {
+                        while (iterator < list.size()) {
+                            currentLog = (LogEntry) list.get(iterator);
+                            bufferedWriter.write(currentLog.getStudentNumber() + separator + currentLog.getDatetime() + currentLog.getPinCode() + "\n");
+                            iterator++;
+                        }
+                    }
+
+                }
             } catch (IOException ex) {
-                
-                TCPServer.logger.log("ERROR: Log lists: cannot close streams");
+                TCPServer.logger.log("ERROR: IO exception when writing lists to file");
+            } finally {
+                try {
+                    bufferedWriter.close();
+                    writer.close();
+                } catch (IOException ex) {
+
+                    TCPServer.logger.log("ERROR: Log lists: cannot close streams");
+                }
             }
         }
+
     }
-    
+
     private void saveFile(List<Object> list, File File, boolean append) {
-        File file = File;
-        FileWriter writer = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            //fix this later
-
-            writer = new FileWriter(file, append);
-            bufferedWriter = new BufferedWriter(writer);
-
-            int iterator = 0;
-
-            if (list.get(0) instanceof Student) {
-                Student currentStudent;
-                synchronized (studentList) {
-                    while (iterator < list.size()) {
-                        currentStudent = (Student) list.get(iterator);
-                        bufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode() + "\n");
-                        iterator++;
-                    }
-                }
-            } else if (list.get(0) instanceof LogEntry) {
-                LogEntry currentLog;
-                synchronized (logList) {
-                    while (iterator < list.size()) {
-                        currentLog = (LogEntry) list.get(iterator);
-                        bufferedWriter.write(currentLog.getStudentNumber() + separator + currentLog.getDatetime() + currentLog.getPinCode() + "\n");
-                        iterator++;
-                    }
-                }
-
-            } else {
-                //if this fires than it means a list of NOT Student or log entry was givin
-                TCPServer.logger.log("CRITICAL: Error both lists are not instances of");
-            }
-
-        } catch (IOException ex) {
-            TCPServer.logger.log("ERROR: IO exception when writing lists to file");
-        } catch (java.lang.IndexOutOfBoundsException e) {
-            TCPServer.logger.log("Lists are empty not saving");
-        } finally {
+        if (list.size() == 0) {
+            TCPServer.logger.log("List is empty not saving");
+        } else {
+            File file = File;
+            FileWriter writer = null;
+            BufferedWriter bufferedWriter = null;
             try {
-                bufferedWriter.close();
-                writer.close();
+                //fix this later
+
+                writer = new FileWriter(file, append);
+                bufferedWriter = new BufferedWriter(writer);
+
+                int iterator = 0;
+
+                if (list.get(0) instanceof Student) {
+
+                    Student currentStudent;
+                    synchronized (studentList) {
+                        while (iterator < list.size()) {
+                            currentStudent = (Student) list.get(iterator);
+                            bufferedWriter.write(currentStudent.getStudentNumber() + separator + currentStudent.getPinCode() + "\n");
+                            iterator++;
+                        }
+                    }
+                }
+
+                iterator = 0;
+
+                if (list.get(0) instanceof LogEntry) {
+                    LogEntry currentLog;
+                    synchronized (logList) {
+                        while (iterator < list.size()) {
+                            currentLog = (LogEntry) list.get(iterator);
+                            bufferedWriter.write(currentLog.getStudentNumber() + separator + currentLog.getDatetime() + currentLog.getPinCode() + "\n");
+                            iterator++;
+                        }
+                    }
+
+                }
             } catch (IOException ex) {
-                
-                TCPServer.logger.log("ERROR: Log lists: cannot close streams");
+                TCPServer.logger.log("ERROR: IO exception when writing lists to file");
+            } finally {
+                try {
+                    bufferedWriter.close();
+                    writer.close();
+                } catch (IOException ex) {
+
+                    TCPServer.logger.log("ERROR: Log lists: cannot close streams");
+                }
             }
         }
-    }
-    
-    void loadFiles() {
-        
+
     }
 
-    /*
-    private void loadFile(List list, String File) {
-        File file = new File(File);
+    void loadFiles() {
+        TCPServer.logger.log("Loading Lists");
+        loadFile(studentList, logList, "studententry.txt", "logentry.txt");
+        TCPServer.logger.log("Lists Loaded");
+    }
+
+    //java does not allow you to check if a generic list is a certain type at runtime which is SO ANOYING
+    //That means i cannot turn the load files into a generic method like the save file methods
+    //there is a work around where you create a new blank list of student and logentry and then
+    //use the instanceof to compare the lists but i refuse to add that extra inefficeny to the program
+    //Actually that wont work, you could try casting the generic list to the blank list and if it throws an
+    //exception try the other type list but once again that adds more cpu cycles to the program
+    //so i decided to just not use generics and do the plain old single method for both lists
+    //it achieves the same thing but with a bit more code :(
+    private void loadFile(List<Student> StudentList, List<LogEntry> LogEntryList, String StudentFile, String LogEntryFile) {
+        File studentFile = new File(StudentFile);
+        File logEntryFile = new File(LogEntryFile);
+
         FileReader reader = null;
         BufferedReader bufferedReader = null;
         try {
             //fix this later
 
-            reader = new FileReader(file);
+            reader = new FileReader(studentFile);
             bufferedReader = new BufferedReader(reader);
 
-            int iterator = 0;
-
-            if (list instanceof Student) {
- 
-                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                    for (String line; (line = br.readLine()) != null;) {
-                        // process the line.
-                        
-                    }
-                    // line is not visible here.
+            String[] data;
+            synchronized (StudentList) {
+                for (String line; (line = bufferedReader.readLine()) != null;) {
+                    data = line.split(separator);
+                    StudentList.add(new Student(data[0], Integer.parseInt(data[1])));
                 }
-
-         
-            } else if (list instanceof LogEntry) {
-                LogEntry currentLog;
-                while (iterator < list.size()) {
-                    currentLog = (LogEntry) list.get(iterator);
-                    bufferedReader.read(currentLog.getStudentNumber() + separator + currentLog.getPinCode());
-
-                }
-
             }
+            reader = new FileReader(logEntryFile);
+            synchronized (LogEntryList) {
+                for (String line; (line = bufferedReader.readLine()) != null;) {
+                    data = line.split(separator);
+                    LogEntryList.add(new LogEntry(data[0], data[1], Integer.parseInt(data[2])));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            TCPServer.logger.log("ERROR: Loading files, files not found");
 
         } catch (IOException ex) {
-            TCPServer.logger.log("ERROR: IO exception when writing lists to file");
+            TCPServer.logger.log("ERROR: IO exception when reading lists from file");
         } finally {
             try {
                 bufferedReader.close();
                 reader.close();
+
             } catch (IOException ex) {
 
                 TCPServer.logger.log("ERROR: Log lists: cannot close streams");
+            } catch (java.lang.NullPointerException e) {
+                TCPServer.logger.log("Error streams dont exist!");
             }
         }
     }
-     */
-    
+
     void exit() {
         exit = true;
     }
-    
+
 }
